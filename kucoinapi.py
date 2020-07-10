ede035aaf6af6720da7f959add7a17b59d0427fb
DESKTOP-S7HUM9C\Tongsang <min35q1@gmail.com>
2020-07-10 10:06:07 +0700

    ordercreate

--- kucoinapi.py
+++ kucoinapi.py
@@ -1,15 +1,20 @@
 import ccxt
 import pandas as pd
-exchange = ccxt.kucoin()
-exchange.apiKey = '5f06d59e3988760006e3efbd'
-echange.passphrase = 'tongsang'
-exchange.secret = '35f65889-d94b-4f57-b930-bbb3940784bc'
 
-exchange.enableRateLimit = True
 
-print(exchange.fetch_balance ()) #look at how much balance
 
-print(exchange.fetchOrders())
+exchange = ccxt.kucoin({
+    'apiKey': '5f06d59e3988760006e3efbd',
+    'secret': '35f65889-d94b-4f57-b930-bbb3940784bc',
+    'password' : 'tongsang'
+})
+
+def ordercreate(status,amount,price):
+    exchange.create_order('BNB/USDT','limit',status,amount,price)
+
+#print(exchange.fetch_balance ()) #look at how much balance
+
+#print(exchange.fetch_total_balance())
 #print(exchange.fetch_ticker('BNB/USD')['last'])
 
-#exhange.create_order('BNB/USD','limit','sell',1,15.7)
+ordercreate('buy',0.001,16.0)
