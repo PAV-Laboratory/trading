e8367ffe720f6f8acca2acb4db9e063fdcb5e639
DESKTOP-S7HUM9C\Tongsang <min35q1@gmail.com>
2020-07-12 14:56:35 +0700

    add comment

--- FTXtrade.py
+++ FTXtrade.py
@@ -3,6 +3,14 @@ import pandas as pd
 import loginFTX
 
 exchange = loginFTX.loginx()
+
+"""
+def loginx():
+    return (ccxt.ftx({
+    'apiKey': 'Your_API',
+    'secret': 'Your_secret',
+    }))
+"""
 def ordercreate(status,amount,price):
     exchange.create_order('XRP-PERP','limit',status,amount,price)
 
