69707163954e64de2b2e1a4109243b2031c2caf7
DESKTOP-S7HUM9C\Tongsang <min35q1@gmail.com>
2020-07-15 14:41:06 +0700

    save14july

--- FTXtrade.py
+++ FTXtrade.py
@@ -1,26 +1,23 @@
 import ccxt
 import pandas as pd
 import numpy as np
+import json
 import time
 import datetime
 import loginFTX
 import connectgoogle
 
 exchange = loginFTX.loginx()
-"""
-def loginx():
-    return (ccxt.ftx({
-    'apiKey': 'Your_API',
-    'secret': 'Your_secret',
-    }))
-"""
-sheet   =  connectgoogle.opensheet();
+sheet   =  connectgoogle.opensheet()
 
 def ordercreate(status,amount,price):
     exchange.create_order('XRP-PERP','limit',status,amount,price)
 
+def addhistory():
+    return (pd.DataFrame(data=exchange.fetch_orders()))
 status = 'buy'
 amount = 1
 price = 0.198500
-
-ordercreate(status,amount,price)
+tradeorder = addhistory()
+a = json.dumps(tradeorder['info'])
+#ordercreate(status,amount,price)
