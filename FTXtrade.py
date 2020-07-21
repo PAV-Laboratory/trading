62fb4e4ba6dcbb87b3d91d5c9df0373119e98376
DESKTOP-S7HUM9C\Tongsang <min35q1@gmail.com>
2020-07-21 13:52:28 +0700

    round up and down

--- FTXtrade.py
+++ FTXtrade.py
@@ -1,6 +1,7 @@
 import ccxt
 import pandas as pd
 import numpy as np
+import math
 import json
 import time
 import datetime
@@ -25,16 +26,24 @@ def checkprice():
     return (pd.DataFrame(data=exchange.fetch_tickers('XRP-PERP')))
 
 def calamount(start_price,start_amount,avg_price,step):
-    return (start_amount+(round(start_price - avg_price,3)) * (step))
-    
+    if((start_price - avg_price)<0):
+        return (start_amount+(math.round_down(start_price - avg_price,3)) * (step))
+    else:
+        return (start_amount+(math.round_up(start_price - avg_price,3)) * (step))
+def round_down(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n * multiplier) / multiplier
+def round_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.ceil(n * multiplier) / multiplier
 position = checkposition() 
 price_xrp   =   checkprice()
 ask_xrp =   price_xrp.iloc[0]
 bid_xrp = price_xrp.iloc[4]
 avg_price = (bid_xrp[0] + ask_xrp[0])/2
-start_price = 0.2000
+start_price = 0.19600
 start_amount = 1000
-step = 20000
+step = 30000
 amount_p = round(position["result"][0]["cost"] * 100) / (avg_price)
 
 
